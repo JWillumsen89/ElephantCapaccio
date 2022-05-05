66a91dbea04afb8bc9958b89e2b2b3bdc2a51ed9
RodrigoPerea <chri65mb@stud.kea.dk>
2022-05-05 10:50:52 +0200

    UI changes

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class UserInterface {
 
   public void getPrice(){
-    System.out.println("Price Calculator\n");
+    System.out.println("\n");
+    System.out.println("Welcome to the price calculator");
+    System.out.println("==========================");
+    System.out.println("\n");
     System.out.println("How many items do you whant to calculate the price on?");
     Scanner userInput = new Scanner(System.in);
     int numbersOfItems = userInput.nextInt();
-    System.out.println("whats the prix pr. unit?");
+    System.out.println("Whats the price pr. unit?");
     double pricePrUnit = userInput.nextDouble();
-    System.out.println("Please type the state code. (Remember the statecode is a tow letter code).U can use:\nUT\nNV\nTX\nAL\nCA");
+    System.out.println("Please type the state code. (Remember the statecode is a tow letter code).U can use following state-codes:\nUT\nNV\nTX\nAL\nCA");
   }
 
 
