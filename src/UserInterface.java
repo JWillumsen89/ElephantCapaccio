6b3557c2fe5dbec097fbfce748d9d18caafebb63
RodrigoPerea <chri65mb@stud.kea.dk>
2022-05-05 10:46:47 +0200

    UI done

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class UserInterface {
+
+  public void getPrice(){
+    System.out.println("Price Calculator\n");
+    System.out.println("How many items do you whant to calculate the price on?");
+    Scanner userInput = new Scanner(System.in);
+    int numbersOfItems = userInput.nextInt();
+    System.out.println("whats the prix pr. unit?");
+    double pricePrUnit = userInput.nextDouble();
+    System.out.println("Please type the state code. (Remember the statecode is a tow letter code).U can use:\nUT\nNV\nTX\nAL\nCA");
+  }
+
+
+
 }
+
+
